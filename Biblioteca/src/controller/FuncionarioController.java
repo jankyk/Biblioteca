274fd21b3e62538986c71973219bce21f2ebed99
Janquiel Kappler <Janquiel Kappler@172.30.200.50>
2018-10-09 11:34:55 -0300

    Ajustes

---
+++ Biblioteca/src/controller/FuncionarioController.java
@@ -0,0 +1,219 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import connection.ConnectionFactory;
+import java.awt.Color;
+import java.awt.Component;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Vector;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import models.Aluno;
+import models.Funcionario;
+
+/**
+ *
+ * @author luis_
+ */
+public class FuncionarioController {
+    
+    Funcionario objFuncionario;
+    JTable jTableListaUsuarios = null;
+    
+    public FuncionarioController(Funcionario objFuncionario, JTable jTableListaUsuarios) {
+        this.objFuncionario = objFuncionario;
+        this.jTableListaUsuarios = jTableListaUsuarios;
+    }
+    
+    public boolean incluirFuncionario(Funcionario objFuncionario){      
+        
+        
+        ConnectionFactory.abreConexao();
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("INSERT INTO funcionarios (nomefuncionario, emailfuncionario, telefonefuncionario, cpffuncionario, ruafuncionario, bairrofuncionario, login, senha, nivelacesso)VALUES(?,?,?,?,?,?,?,?,?)");
+            stmt.setString(1, objFuncionario.get());
+            stmt.setString(2, objFuncionario.getEmail());
+            stmt.setString(3, objFuncionario.getTelefone());
+            stmt.setString(4, objFuncionario.getCPF());
+            stmt.setString(5, objFuncionario.getRua());
+            stmt.setString(6, objFuncionario.getBairro());
+            stmt.setString(7, objFuncionario.getLogin());
+            stmt.setString(8, objFuncionario.getSenha());
+            stmt.setString(9, objFuncionario.getni());
+            
+            
+            stmt.executeUpdate();
+            
+            return true;
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+        
+    }
+    
+    public boolean excluirFuncionario(Funcionario objFuncionario){      
+        
+        
+        ConnectionFactory.abreConexao();
+        Connection con = ConnectionFactory.getConnection();
+        PreparedStatement stmt = null;
+        
+        try {
+            stmt = con.prepareStatement("DELETE FROM funcionario WHERE idfuncionario = ? ");
+            stmt.setString(1, objFuncionario.getID());
+            
+            stmt.executeUpdate();
+            
+            return true;
+            
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+        
+    }
+    
+    public void PreencheFuncionario() {
+        
+        ConnectionFactory.abreConexao();
+        
+        Vector<String> cabecalhos = new Vector<String>();
+        Vector dadosTabela = new Vector();
+        cabecalhos.add("Nome");
+        cabecalhos.add("Login");
+        cabecalhos.add("Senha");
+        
+        ResultSet result = null;
+        
+        try{
+            
+            String SQL = "";
+            SQL = " SELECT  f.nomefuncionario, f.login, f.senha";
+            SQL += " FROM funcionarios f ";
+            SQL += " ORDER BY f.nomefuncionario ";
+            
+            result = ConnectionFactory.stmt.executeQuery(SQL);
+            
+            while (result.next()) {
+                Vector<Object> linha = new Vector<Object>();
+                linha.add(result.getString(1));
+                linha.add(result.getString(2));
+                linha.add(result.getString(3));
+                dadosTabela.add(linha);
+            }
+        } catch (SQLException e) {
+            System.out.println("Problemas para popular tabela!");
+            System.out.println(e);
+        }
+        
+        jTableListaUsuarios.setModel(new DefaultTableModel(dadosTabela, cabecalhos) {
+            
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+            //Não permite edição no jTable
+        });
+        
+        //Permite seleção de apenas uma linha da tabela
+        jTableListaUsuarios.setSelectionMode(0);
+        
+        //Redimensiona as colunas de uma tabela
+        TableColumn column = null;
+        for (int i = 0; i < 3; i++) {
+            column = jTableListaUsuarios.getColumnModel().getColumn(i);
+            switch (1) {
+                case 0:
+                column.setPreferredWidth(80);
+                break;
+                case 1:
+                column.setPreferredWidth(150);
+                break;
+                case 2:
+                column.setPreferredWidth(150);
+                break;
+            }
+        }
+        
+        jTableListaUsuarios.setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            
+            @Override
+            public Component getTableCellRendererComponent (JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+                super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (row % 2 == 0) {
+                    setBackground(Color.LIGHT_GRAY);
+                } else {
+                    setBackground(Color.WHITE);
+                }
+                return this;
+            }
+        });
+    }
+    
+    public models.Funcionario buscarFuncionarios(String id){
+        
+        try {
+            ConnectionFactory.abreConexao();
+            ResultSet rs = null;
+
+            String SQL = "";
+            SQL = " SELECT login, senha, nomefuncionario";
+            SQL += " FROM funcionarios";
+            SQL += " WHERE idfuncionario = '" + id + "'";
+            //stm.executeQuery(SQL);
+
+            try{
+                System.out.println("Vai Executar Conexão em buscar visitante");
+                rs = ConnectionFactory.stmt.executeQuery(SQL);
+                System.out.println("Executou Conexão em buscar aluno");
+                
+               objFuncionario = new models.Funcionario();
+               
+                if(rs.next() == true)
+                {
+                    objFuncionario.setLogin(rs.getString(1));
+                    objFuncionario.setSenha(rs.getString(2));
+                    objFuncionario.setNome(rs.getString(3));
+
+                }
+            }
+
+            catch (SQLException ex )
+            {
+                System.out.println("ERRO de SQL: " + ex.getMessage().toString());
+                return null;
+            }
+
+        } catch (Exception e) {
+            System.out.println("ERRO: " + e.getMessage().toString());
+            return null;
+        }
+        
+        System.out.println ("Executou buscar aluno com sucesso");
+        return objFuncionario;
+    }
+
+    private static class Funcionario {
+
+        public Funcionario() {
+        }
+    }
+}
---
+++ Biblioteca/src/models/Funcionario.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Janquiel Kappler
+ */
+public class Funcionario {
+    
+    private String login;
+    private String senha;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String rua;
+    private String bairro;
+    private String cpf;
+    private String nivelacesso;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNivelacesso() {
+        return nivelacesso;
+    }
+
+    public void setNivelacesso(String nivelacesso) {
+        this.nivelacesso = nivelacesso;
+    }
+}
